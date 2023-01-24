af4afa9999adee63ced95c695e0a6f0bd85cd233
Bruno Vicente <brunovicente.dev@gmail.com>
2023-01-24 14:52:01 -0300

    Added delete icon

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,8 +1,14 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
-import { withOrientation } from "react-navigation";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -16,8 +22,13 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           return (
             <View style={styles.Post}>
-              <Text style={styles.Title}> {item.title} </Text>
-              <Feather name="trash" style={styles.Icon} />
+              <Text style={styles.Title}>
+                {" "}
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => console.log(item.id)}>
+                <Feather name="trash" style={styles.Icon} />
+              </TouchableOpacity>
             </View>
           );
         }}

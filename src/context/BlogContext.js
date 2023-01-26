15ed44c8dd5a17b7d5a1830f41b530aeda527b67
Bruno Vicente <brunovicente.dev@gmail.com>
2023-01-26 15:35:36 -0300

    Added editing and navigation to EditScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -16,6 +16,10 @@ const blogReducer = (state, action) => {
           content: action.payload.content,
         },
       ];
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     default:
       return state;
   }
@@ -24,7 +28,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
 
@@ -34,8 +40,17 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   []
 );
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,7 +4,7 @@ import { Context } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, editBlogPost } = useContext(Context);
 
   const blogPost = state.find(
     (blogPost) => blogPost.id === navigation.getParam("id")
@@ -17,7 +17,7 @@ const EditScreen = ({ navigation }) => {
       TitleButton="Edit Post"
       InitialValues={{ title: blogPost.title, content: blogPost.content }}
       onSubmit={(title, content) => {
-        console.log(title, content);
+        editBlogPost(blogPost.id, title, content, () => navigation.goBack());
       }}
     />
   );

f5526029026221a07f2ee1a37d21a5e8a880ad97
Bruno Vicente <brunovicente.dev@gmail.com>
2023-01-27 15:29:25 -0300

    Another baseURL

--- src/api/blogServer.js
+++ src/api/blogServer.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://852b-2804-1b3-a240-dc17-e142-a472-8f85-420.sa.ngrok.io/",
+  baseURL: "https://5696-2804-1b3-a240-dc17-e142-a472-8f85-420.sa.ngrok.io",
 });
